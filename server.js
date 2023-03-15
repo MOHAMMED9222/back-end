c6506c69056406c08e2abf69e13a66588722a779
Mohammed Abubakar <mohammedfahim363@yahoo.com>
2023-03-14 19:32:41 -0700

    day2backend

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 //middleware
 app.use(cors());
 const Book = require('./modules/Book.js');
+// we must have this to recieve JSON data from a request
+app.use(express.json());
 
 const mongoose = require('mongoose');
 // add validation to confirm we are wired up to our mongo DB
@@ -22,23 +24,58 @@ mongoose.connect(process.env.DB_URL);
 // define port validate env is working
 const PORT = process.env.PORT || 3001;
 
-// routes
+// ROUTES
 app.get('/test', (request, response) => {
 
   response.send('test request received')
 
 })
 
-// ROUTES
+
 app.get('/', (request, response) => {
   response.status(200).send('Welcome!');
 });
+app.get('/Book', getBook);
+app.post('/Book', postBook)
+// 'id' is the variable I declared here: path parameter
+// If I have this URL coming in the request:
+// http://localhost:3001/cats/637bceabc57c693faee21e8f
+// I access the value 637bceabc57c693faee21e8f with
+// req.params.id
+// 'id' is the variable I declared here:
+app.delete('/Book/:id', deleteBook);
 
 app.get('/Book', getBook);
 async function getBook(req, res, next) {
   try {
     let results = await Book.find({});
     res.status(200).send(results);
+  } catch(err) {
+    next(err);
+  }
+}
+
+async function deleteBook(req, res, next) {
+  // console.log(req);
+  // try catch = basic scaffling
+  try {
+    let id = req.params.id;
+    // do not expect anything to be returned by findByIdAndDelete
+    await Book.findByIdAndDelete(id);
+    res.status(200).send('Book deleted');
+    // find out where validation goes
+  } catch(err) {
+    next(err);
+  }
+}
+
+
+async function postBook(req, res, next) {
+  console.log(req.body);
+  try {
+    // we want to add cats to our database
+    let createdBook = await Book.create(req.body);
+    res.status(200).send(createdBook);
   } catch(err) {
     next(err);
   }
